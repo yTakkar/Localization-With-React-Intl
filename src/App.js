16f36f515b9bd9f8f78c6310a3238d8ee8f565be
Faiyaz Shaikh <www.shtakkar@gmail.com>
2018-06-26 17:43:31 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ChangeLocale from './changeLocale'
-import Book from './components/Book'
+import Book from './Book/Book'
 import Visit from './visit'
 
 class App extends React.Component {
