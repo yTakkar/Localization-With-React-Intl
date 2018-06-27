e3d6b8d31c43658ecae64595b89a683db38d2a29
Faiyaz Shaikh <www.shtakkar@gmail.com>
2018-06-27 15:59:24 +0530

    Some changes in date

--- src/Book/published.js
+++ src/Book/published.js
@@ -9,7 +9,7 @@ const Published = () => {
       <h4>Published Time:</h4>
       <span>
         <FormattedDate value={time} day="2-digit" month="long" year="numeric" />{' '}
-        <FormattedTime value={time} />
+        <FormattedTime value={new Date(1514708748162)} />
       </span>
       <br />
       <span>
