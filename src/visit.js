589293a9237df3b65ea722ecece7558df79ae129
Faiyaz Shaikh <www.shtakkar@gmail.com>
2018-07-06 14:44:16 +0530

    target='_blank'

--- src/visit.js
+++ src/visit.js
@@ -3,9 +3,11 @@ import React from 'react'
 const Visit = () => (
   <div>
     <span>
-      <a href="https://codesandbox.io/s/llwk0rrk97">Visit</a> for localization
-      with{' '}
-      <a href="https://github.com/stefalda/react-localization">
+      <a target="_blank" href="https://codesandbox.io/s/llwk0rrk97">
+        Visit
+      </a>{' '}
+      for localization with{' '}
+      <a target="_blank" href="https://github.com/stefalda/react-localization">
         react-localization
       </a>
     </span>
